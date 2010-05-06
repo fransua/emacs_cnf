7b2fc1e442d767d57b3b089e03c5ffc237fddcf1
garamonfok <serra.francois@gmail.com>
2010-05-06 20:58:57 +0200

    first emacs.el

--- emacs.el
+++ emacs.el
@@ -1,6 +1,5 @@
 (setq load-path (cons "~/.emacs.git/emacs.d" load-path))
-(require 'edit-server)
-(edit-server-start)
+
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
